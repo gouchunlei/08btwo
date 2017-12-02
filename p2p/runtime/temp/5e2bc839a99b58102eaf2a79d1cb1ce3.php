562895abd4b5116d14eab75f0d35921341e2ef49
tannawen <664018352@qq.com>
2017-12-02 13:34:24 +0800

    1

---
+++ p2p/runtime/temp/5e2bc839a99b58102eaf2a79d1cb1ce3.php
@@ -0,0 +1,39 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:67:"F:\phpstudy\WWW\git\public/../application/index\view\myself\my.html";i:1512174305;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="format-detection" content="telephone=no,email=no,adress=no"/>
+<title>首页</title>
+<link rel="stylesheet" type="text/css" href="__CSS__/common.css" />
+<script type="text/javascript" src="__JS__/jquery.js"></script>
+</head>
+<body>
+<!-- 页面头部 -->
+<section>
+  <div class="navigation-bar"> <a href="#" class="z-04"></a> <span>会员中心</span> <a href="#" class="z-03"></a> </div>
+</section>
+<!-- 中间内容 -->
+<div class="summary">
+  <div class="head"><img id="photoImage" src="__Iages__/avatar1.png" alt="" style="width:80px;height:80px;z-index:0;"> <span class="username">zhuchaoyou</span>
+    <div class="headamount">
+      <div class="p-amount"> <i>0.00 </i><br>
+        累计收益（元） </div>
+      <div class="p-amount"> <i>
+        <label> 0.00</label>
+        </i><br>
+        可用余额（元） </div>
+    </div>
+  </div>
+</div>
+<div class="personal">
+  <div class="personal-list"> <a class="plist" href="recharge"> <span class="picon1"></span><span class="ptit">充值</span><span class="iconarrow"></span> </a> <a class="plist" href="invest"> <span class="picon2"></span><span class="ptit">投资记录</span><span class="iconarrow"></span> </a> <a class="plist" href="borrowing"> <span class="picon7"></span><span class="ptit">借款记录</span><span class="iconarrow"></span> </a><a class="plist" href="withdrawal"> <span class="picon3"></span><span class="ptit">提现</span><span class="iconarrow"></span> </a> </div>
+  <div class="personalout"> <a class="plist" href="message"> <span class="picon5"></span><span class="ptit">系统消息（<i class="pink">0</i> 条未读）</span><span class="iconarrow"></span> </a> </div>
+  <div class="personalout"> <a class="plist" href="security"> <span class="picon4"></span><span class="ptit">安全设置</span><span class="iconarrow"></span> </a> </div>
+  <div class="personalout"> <a class="plist" href="#"> <span class="picon6"></span><span class="ptit">退出账号</span><span class="iconarrow"></span> </a> </div>
+</div>
+<!-- 页面底部 -->
+<nav class="footer border_t" id="footer"> <a href="index.html" class="" id="jx"><span></span>首页</a> <a href="list.html" id="lc" class="nav_on"><span></span>理财</a> <a href="protocol" id="jk" class=""><span></span>借款</a> <a href="个人中心.html" id="cf" class=""><span></span>我的账户</a> </nav>
+</body>
+</html>
---
+++ p2p/runtime/temp/db080e22bb2fbc1bf36cfeebf3864cf8.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:68:"F:\phpstudy\WWW\git\public/../application/index\view\index\show.html";i:1512115876;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>fasdfadsf</title>
+</head>
+<body>
+	<?=$name?>
+</body>
+</html>

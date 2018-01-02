6a82ab6328eca044a80c9074a490ddcc08e25074
gouchunlei <2231205846@qq.com>
2018-01-02 16:06:51 +0800

    add a file

--- laravel/resources/views/login/Regist.blade.php
+++ laravel/resources/views/login/Regist.blade.php
@@ -13,7 +13,22 @@
 	<script type="text/javascript" src="js/jquery.bxslider_e88acd1b.js"></script>
     
     <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="js/menu.js"></script>
+    <script type="text/javascript" src="js/menu.js"></script>    
+        
+	<script type="text/javascript" src="js/select.js"></script>
+    
+	<script type="text/javascript" src="js/lrscroll.js"></script>
+    
+    <script type="text/javascript" src="js/iban.js"></script>
+    <script type="text/javascript" src="js/fban.js"></script>
+    <script type="text/javascript" src="js/f_ban.js"></script>
+    <script type="text/javascript" src="js/mban.js"></script>
+    <script type="text/javascript" src="js/bban.js"></script>
+    <script type="text/javascript" src="js/hban.js"></script>
+    <script type="text/javascript" src="js/tban.js"></script>
+    
+	<script type="text/javascript" src="js/lrscroll_1.js"></script>
+    
     
 <title>尤洪</title>
 </head>
@@ -49,23 +64,31 @@
               </tr>
               <tr height="50">
                 <td align="right"><font color="#ff4e00">*</font>&nbsp;用户名 &nbsp;</td>
-                <td><input type="text" value="" class="l_user username" /></td>
+                <td><input type="text" value="" class="l_user" /></td>
               </tr>
               <tr height="50">
                 <td align="right"><font color="#ff4e00">*</font>&nbsp;密码 &nbsp;</td>
-                <td><input type="password" value="" class="l_pwd one_pwd" /></td>
+                <td><input type="password" value="" class="l_pwd" /></td>
               </tr>
               <tr height="50">
                 <td align="right"><font color="#ff4e00">*</font>&nbsp;确认密码 &nbsp;</td>
-                <td><input type="password" value="" class="l_pwd two_pwd" /></td>
+                <td><input type="password" value="" class="l_pwd" /></td>
               </tr>
               <tr height="50">
                 <td align="right"><font color="#ff4e00">*</font>&nbsp;邮箱 &nbsp;</td>
-                <td><input type="text" value="" class="l_email email" /></td>
+                <td><input type="text" value="" class="l_email" /></td>
               </tr>
               <tr height="50">
                 <td align="right"><font color="#ff4e00">*</font>&nbsp;手机 &nbsp;</td>
-                <td><input type="text" value="" class="l_tel phone" /></td>
+                <td><input type="text" value="" class="l_tel" /></td>
+              </tr>
+              <tr height="50">
+                <td align="right">邀请人会员名 &nbsp;</td>
+                <td><input type="text" value="" class="l_mem" /></td>
+              </tr>
+              <tr height="50">
+                <td align="right">邀请人ID号 &nbsp;</td>
+                <td><input type="text" value="" class="l_num" /></td>
               </tr>
               <tr height="50">
                 <td align="right"> <font color="#ff4e00">*</font>&nbsp;验证码 &nbsp;</td>
@@ -100,7 +123,10 @@
     </div>    	
 </div>
 <!--End Footer End -->    
+
 </body>
+
+
 <!--[if IE 6]>
 <script src="//letskillie6.googlecode.com/svn/trunk/2/zh_CN.js"></script>
 <![endif]-->

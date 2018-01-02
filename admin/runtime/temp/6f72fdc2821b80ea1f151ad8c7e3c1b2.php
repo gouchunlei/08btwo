169f6b031eae10a1826f9be456337f336f47f4de
gouchunlei <2231205846@qq.com>
2018-01-02 18:53:57 +0800

    12

---
+++ admin/runtime/temp/6f72fdc2821b80ea1f151ad8c7e3c1b2.php
@@ -0,0 +1,201 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:77:"F:\phpstudy\WWW\sugoo\admin\public/../application/index\view\vip\viplist.html";i:1514880905;}*/ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>DouPHP 管理中心</title>
+<meta name="Copyright" content="Douco Design." />
+<link href="__CSS__public.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="__JS__jquery.min.js"></script>
+<script type="text/javascript" src="__JS__global.js"></script>
+</head>
+<body>
+<div id="dcWrap"> <div id="dcHead">
+ <div id="head">
+  <div class="logo"><a href="index.html"><img src="__IMG__dclogo.gif" alt="logo"></a></div>
+  <div class="nav">
+   <ul>
+    <li class="M"><a href="JavaScript:void(0);" class="topAdd">新建</a>
+     <div class="drop mTopad"><a href="product.php?rec=add">商品</a> <a href="article.php?rec=add">评论</a> <a href="show.html">首页幻灯</a>  <a href="manager.php?rec=add">管理员</a> <a href="link.html"></a> </div>
+    </li>
+   </ul>
+   <ul class="navRight">
+    <li class="M noLeft"><a href="JavaScript:void(0);">您好，admin</a>
+     <div class="drop mUser">
+      <a href="manager.php?rec=edit&id=1">编辑我的个人资料</a>
+      <a href="manager.php?rec=cloud_account">设置云账户</a>
+     </div>
+    </li>
+    <li class="noRight"><a href="login.php?rec=logout">退出</a></li>
+   </ul>
+  </div>
+ </div>
+</div>
+<!-- dcHead 结束 --> <div id="dcLeft"><div id="menu">
+ <ul class="top">
+  <li><a href="<?php echo url('index/index/index'); ?>"><i class="home"></i><em>管理首页</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/system/index'); ?>"><i class="system"></i><em>系统设置</em></a></li>
+  <li><a href="<?php echo url('index/show/index'); ?>"><i class="show"></i><em>首页幻灯广告</em></a></li>
+ </ul>
+   <ul>
+  <li><a href="<?php echo url('index/goods/index'); ?>"><i class="productCat"></i><em>商品管理</em></a></li>
+ </ul>
+  <ul>
+  <li><a href="<?php echo url('index/Category/index'); ?>"><i class="productCat"></i><em>商品分类</em></a></li>
+ </ul>
+  <ul>
+  <li><a href="<?php echo url('index/attr/index'); ?>"><i class="productCat"></i><em>商品属性</em></a></li>
+ </ul>
+  <ul>
+  <li><a href="<?php echo url('index/brand/index'); ?>"><i class="productCat"></i><em>商品品牌</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/vip/index'); ?>"><i class="productCat"></i><em>会员管理</em></a></li>
+  <li><a href="<?php echo url('index/vip/log'); ?>"><i class="product"></i><em>会员列表</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/order/index'); ?>"><i class="productCat"></i><em>订单管理</em></a></li>
+  <li><a href="<?php echo url('index/order/product'); ?>"><i class="product"></i><em>订单列表</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/logistics/index'); ?>"><i class="productCat"></i><em>物流管理</em></a></li>
+  <li><a href="<?php echo url('index/logistics/product'); ?>"><i class="product"></i><em>物流列表</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/trading/index'); ?>"><i class="productCat"></i><em>交易管理</em></a></li>
+  <li><a href="<?php echo url('index/trading/product'); ?>"><i class="product"></i><em>交易列表</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/pay/index'); ?>"><i class="productCat"></i><em>支付管理</em></a></li>
+  <li><a href="<?php echo url('index/pay/product'); ?>"><i class="product"></i><em>支付列表</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/money/index'); ?>"><i class="productCat"></i><em>平台资金管理</em></a></li>
+  <li><a href="<?php echo url('index/money/product'); ?>"><i class="product"></i><em>平台资金列表</em></a></li>
+ </ul>
+ <ul>
+  <li><a href="<?php echo url('index/marketing/index'); ?>"><i class="productCat"></i><em>营销管理</em></a></li>
+  <li><a href="<?php echo url('index/marketing/product'); ?>"><i class="product"></i><em>营销列表</em></a></li>
+ </ul>
+  <ul>
+  <li><a href="<?php echo url('index/talk/index'); ?>"><i class="productCat"></i><em>评论管理</em></a></li>
+  <li><a href="<?php echo url('index/talk/add'); ?>"><i class="product"></i><em>评论列表</em></a></li>
+ </ul>
+   <ul class="bot">
+  <li><a href="<?php echo url('index/Manager/index'); ?>"><i class="manager"></i><em>网站管理员</em></a></li>
+  <li><a href="<?php echo url('index/Manager/log'); ?>"><i class="managerLog"></i><em>操作记录</em></a></li>
+ </ul>
+</div></div>
+ <div id="dcMain">
+   <!-- 当前位置 -->
+<div id="urHere">DouPHP 管理中心<b>></b><strong>操作记录</strong> </div>   <div id="manager" class="mainBox" style="height:auto!important;height:550px;min-height:550px;">
+    <h3>操作记录</h3>
+                    <table width="100%" border="0" cellpadding="8" cellspacing="0" class="tableBasic">
+     <tr>
+      <th width="30" align="center">编号</th>
+      <th width="150" align="left">操作时间</th>
+      <th width="100" align="center">操作者</th>
+      <th align="left">操作记录</th>
+      <th width="100" align="center">IP地址</th>
+     </tr>
+          <tr>
+      <td align="center">12</td>
+      <td>2016-02-26 20:53:17</td>
+      <td align="center">admin</td>
+      <td align="left">管理员登录: 登录成功！</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">11</td>
+      <td>2016-02-25 23:29:08</td>
+      <td align="center">admin</td>
+      <td align="left">管理员登录: 登录成功！</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">10</td>
+      <td>2016-02-25 13:48:48</td>
+      <td align="center">admin</td>
+      <td align="left">管理员登录: 登录成功！</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">9</td>
+      <td>2013-10-16 09:43:01</td>
+      <td align="center">admin</td>
+      <td align="left">编辑单页面: 营销网络</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">8</td>
+      <td>2013-10-16 09:42:58</td>
+      <td align="center">admin</td>
+      <td align="left">编辑单页面: 公司简介</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">7</td>
+      <td>2013-10-16 09:42:55</td>
+      <td align="center">admin</td>
+      <td align="left">编辑单页面: 企业荣誉</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">6</td>
+      <td>2013-10-16 09:42:39</td>
+      <td align="center">admin</td>
+      <td align="left">编辑文章分类: 公司动态</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">5</td>
+      <td>2013-10-16 09:42:36</td>
+      <td align="center">admin</td>
+      <td align="left">编辑商品分类: 电子数码</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">4</td>
+      <td>2013-10-16 09:42:33</td>
+      <td align="center">admin</td>
+      <td align="left">编辑单页面: 联系我们</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">3</td>
+      <td>2013-10-16 09:42:29</td>
+      <td align="center">admin</td>
+      <td align="left">编辑幻灯: 广告图片01</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">2</td>
+      <td>2013-10-16 09:42:25</td>
+      <td align="center">admin</td>
+      <td align="left">编辑导航: 公司简介</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+          <tr>
+      <td align="center">1</td>
+      <td>2013-10-16 09:42:20</td>
+      <td align="center">admin</td>
+      <td align="left">系统设置: 编辑成功</td>
+      <td align="center">127.0.0.1</td>
+     </tr>
+         </table>
+    <div class="pager">总计 12 个记录，共 1 页，当前第 1 页 | <a href="manager.php?rec=manager_log&page=1">第一页</a> 上一页 下一页 <a href="manager.php?rec=manager_log&page=1">最末页</a></div>           </div>
+ </div>
+ <div class="clear"></div>
+<div id="dcFooter">
+ <div id="footer">
+  <div class="line"></div>
+  <ul>
+   版权所有 © 2013-2015 漳州豆壳网络科技有限公司，并保留所有权利。
+  </ul>
+ </div>
+</div><!-- dcFooter 结束 -->
+<div class="clear"></div> </div>
+</body>
+</html>
